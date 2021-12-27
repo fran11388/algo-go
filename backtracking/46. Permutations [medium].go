25e83309d42cb14a37db3e4d7244c22517dfe884
frank <fran11388@gmail.com>
2021-12-28 02:00:04 +0800

    permute by forloop

--- backtracking/46. Permutations [medium].go
+++ backtracking/46. Permutations [medium].go
@@ -7,6 +7,11 @@ func main() {
 }
 
 func permute(nums []int) [][]int {
+	//return permuteByBacktracking(nums)
+	return permuteByForLoop(nums)
+}
+
+func permuteByBacktracking(nums []int) [][]int {
 	perms := []int{}
 	used := make([]bool, len(nums))
 	result := [][]int{}
@@ -36,4 +41,35 @@ func backtrack(nums []int, perms *[]int, used []bool, result *[][]int) {
 		used[i] = false
 		*perms = (*perms)[:len(*perms)-1] //remove last element
 	}
+}
+
+func permuteByForLoop(nums []int) [][]int {
+	result := [][]int{{}}
+	for _, n := range nums {
+		result = placeNumber(result, n)
+	}
+	return result
+}
+
+func placeNumber(arr [][]int, n int) [][]int {
+	result := [][]int{}
+
+	for _, list := range arr {
+		tmpResult := make([]int, 0, len(list)+1)
+		tmpResult = append(tmpResult, n)
+		tmpResult = append(tmpResult, list...)
+
+		//add to result
+		res := make([]int, len(tmpResult))
+		copy(res, tmpResult)
+		result = append(result, res)
+		for i := 0; i < len(tmpResult)-1; i++ { //移動n-1次(第一個元素依序移動到尾)
+			tmpResult[i], tmpResult[i+1] = tmpResult[i+1], tmpResult[i]
+			res = make([]int, len(tmpResult))
+			copy(res, tmpResult)
+			result = append(result, res)
+		}
+
+	}
+	return result
 }
