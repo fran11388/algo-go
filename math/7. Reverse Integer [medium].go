8b7251fbf529ee9eb08568ad23198848fde94ace
frank <fran11388@gmail.com>
2022-01-07 00:33:29 +0800

    7. Reverse Integer [medium].go

---
+++ math/7. Reverse Integer [medium].go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main(){
+	n:=123
+	fmt.Println(reverse(n))
+	n=-123
+	fmt.Println(reverse(n))
+}
+func reverse(x int) int {
+	res := 0
+	for x!=0{
+		pop:=x%10
+		x=x/10
+
+		if res>math.MaxInt32/10||(res==math.MaxInt32/10&&pop>7){
+			return 0
+		}
+		if res<math.MinInt32/10||(res==math.MinInt32/10&&pop<(-8)){
+			return 0
+		}
+
+		res=res*10+pop
+	}
+	return res
+}
