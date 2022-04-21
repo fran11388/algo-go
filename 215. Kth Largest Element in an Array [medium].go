a343bd89f52c58a32bc3b5c55e30c7cec67b0cf8
frank_yan <fran11388@gmail.com>
2022-04-22 02:34:47 +0800

    215. Kth Largest Element in an Array [medium].go

--- 215. Kth Largest Element in an Array [medium].go
+++ 215. Kth Largest Element in an Array [medium].go
@@ -7,10 +7,44 @@ import (
 
 func main(){
 	nums:=[]int{3,2,1,5,6,4}
-	fmt.Println(findKthLargest(nums,2))
+	fmt.Println(findKthLargestByQuickSelect(nums,2))
 }
 
-func findKthLargest(nums []int, k int) int {
+//kth largest會在n-k的位置(小到大)
+//不斷找pivot直到pivot位在n-k
+func findKthLargestByQuickSelect(nums []int, k int) int {
+	targetIdx:=len(nums)-k
+	start,end:=0,len(nums)-1
+	for true{
+		pivotIdx:=findPivot(nums,start,end)
+		if pivotIdx==targetIdx{
+			return nums[pivotIdx]
+		}else if targetIdx>pivotIdx{
+			start=pivotIdx+1
+		}else{
+			end=pivotIdx-1
+		}
+	}
+	return -1
+}
+
+func findPivot(nums []int, start, end int) int {
+	randomPivot:=end
+	i,j:=start-1,start
+	for ;j<randomPivot;j++{
+		n:=nums[j]
+		if n<nums[randomPivot]{
+			nums[i+1],nums[j]=nums[j],nums[i+1]
+			i++
+		}
+	}
+
+	nums[i+1],nums[randomPivot]= nums[randomPivot],nums[i+1]
+	return i+1
+}
+
+
+func findKthLargestByHeap(nums []int, k int) int {
 	heap := &MinHeap{}
 	for _, n := range nums {
 		heap.Insert(n)  //注意  這邊要先insert，有超過才remove，這樣remove掉的才會是小的
